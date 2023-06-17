a78b0d48515f241524d943b7c9513208f4ffa02f
Moin <moinshaikh1592001@gmail.com>
2023-06-17 15:30:54 +0530

    Initial Commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,110 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+import "../styles/index.css"
+import { useState } from "react";
+
+
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix" },
+  ];
+};
+const Index: React.FC = () => {
+  let [showPie, setShowPie] = useState<number>(0)
+
+  const handleClick = (num: number): void => {
+
+    setShowPie(num);
+    setTimeout(() => {
+      setShowPie(0);
+    }, 1500);
+  };
+
+
+  return (
+    <div className="main">
+
+      <div className="message-container">
+        <h1>404</h1>
+        <p>It look like we couldn't find this page <br /> To make it up to you, please choose a co-founder to pie in the face</p>
+      </div>
+
+
+
+      <div className="avatar-container">
+        <div className="avatar-div">
+          <img
+            src="/images/baby1.jpg"
+            alt="Co-Founder 1"
+            onClick={() => { handleClick(1) }}
+            className={`avatar ${showPie === 1 ? "hit" : ""}`}
+
+          />
+          Thomas
+
+          {showPie === 1 && (
+            <div className="pie-animation">
+              <div className="pie-tin" >
+                <img src="/images/pie-tin.jpg" alt="pie-tin" className="pie-tin" />
+              </div>
+
+            </div>
+          )}
+        </div>
+        <div className="avatar-div">
+          <img
+            src="/images/baby2.jpg"
+            alt="Co-Founder 2"
+            onClick={() => { handleClick(2) }}
+            className={`avatar ${showPie === 2 ? "hit" : ""}`}
+          />
+          Ryan
+          {showPie === 2 && (
+            <div className="pie-animation">
+              <div className="pie-tin" >
+                <img src="/images/pie-tin.jpg" alt="pie-tin" className="pie-tin" /></div>
+            </div>
+          )}
+        </div>
+
+        <div className="avatar-div">
+          <img
+            src="/images/baby3.jpg"
+            alt="Co-Founder 3"
+            onClick={() => { handleClick(3) }}
+            className={`avatar ${showPie === 3 ? "hit" : ""}`}
+          />
+          Emma
+          {showPie === 3 && (
+            <div className="pie-animation">
+              <div className="pie-tin" >
+                <img src="/images/pie-tin.jpg" alt="pie-tin" className="pie-tin" /></div>
+            </div>
+          )}
+        </div>
+
+        <div className="avatar-div">
+          <img
+            src="/images/baby4.jpg"
+            alt="Co-Founder 4"
+            onClick={() => { handleClick(4) }}
+            className={`avatar ${showPie === 4 ? "hit" : ""}`}
+          />
+          Micheal
+          {showPie === 4 && (
+            <div className="pie-animation">
+              <div className="pie-tin" >
+                <img src="/images/pie-tin.jpg" alt="pie-tin" className="pie-tin" /></div>
+            </div>
+          )}
+        </div>
+
+
+      </div>
+
+    </div>
+  );
+}
+
+
+export default Index
---
+++ remix.config.js
@@ -0,0 +1,34 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
+  serverModuleFormat: "cjs",
+  future: {
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
+  getLoaders() {
+    return [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: require.resolve("style-loader"),
+          },
+          {
+            loader: require.resolve("css-loader"),
+            options: {
+              importLoaders: 1,
+            },
+          },
+        ],
+      },
+    ];
+  },
+};
