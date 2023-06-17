057af0843a4bea92b978fdc77cb6736a2167c207
Moin <moinshaikh1592001@gmail.com>
2023-06-17 16:20:03 +0530

    added links to stylesheet

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,12 @@
 import type { V2_MetaFunction } from "@remix-run/node";
 
 import { useState } from "react";
-import '../styles/index.css'
+
+import styles from "~/styles/index.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
 
 export const meta: V2_MetaFunction = () => {
   return [
